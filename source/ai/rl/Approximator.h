3fbfe520f9fe362690e0dcb5ae151b33341d993b
Sergio Tessaris <39191293+stessaris@users.noreply.github.com>
2018-09-20 11:09:02 +0000

    Fixes Boost unordered_map serialization

--- source/ai/rl/Approximator.h
+++ source/ai/rl/Approximator.h
@@ -11,77 +11,11 @@
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/map.hpp>
+#include <boost/serialization/unordered_map.hpp>
 
 #include "core/Common.h"
 #include "ai/AI.h"
 #include "core/HashMap.h"
-
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// serialization/map.hpp:
-// serialization for stl map templates
-
-// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com .
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-// See http://www.boost.org for updates, documentation, and revision history.
-
-#include <boost/unordered_map.hpp>
-
-#include <boost/config.hpp>
-
-#include <boost/serialization/utility.hpp>
-#include <boost/serialization/collections_save_imp.hpp>
-#include <boost/serialization/collections_load_imp.hpp>
-#include <boost/serialization/split_free.hpp>
-
-namespace boost {
-    namespace serialization {
-
-        template<class Archive, class Type, class Key, class Hash, class Compare, class Allocator >
-            inline void save(
-                             Archive & ar,
-                             const boost::unordered_map<Key, Type, Hash, Compare, Allocator> &t,
-                             const unsigned int /* file_version */
-                             ){
-            boost::serialization::stl::save_collection<
-                Archive,
-                boost::unordered_map<Key, Type, Hash, Compare, Allocator>
-                >(ar, t);
-        }
-
-        template<class Archive, class Type, class Key, class Hash, class Compare, class Allocator >
-            inline void load(
-                             Archive & ar,
-                             boost::unordered_map<Key, Type, Hash, Compare, Allocator> &t,
-                             const unsigned int /* file_version */
-                             ){
-            boost::serialization::stl::load_collection<
-                Archive,
-                boost::unordered_map<Key, Type, Hash, Compare, Allocator>,
-                boost::serialization::stl::archive_input_map<
-                Archive, boost::unordered_map<Key, Type, Hash, Compare, Allocator> >,
-                boost::serialization::stl::no_reserve_imp<boost::unordered_map<
-                Key, Type, Hash, Compare, Allocator
-                >
-                >
-                >(ar, t);
-        }
-
-        // split non-intrusive serialization function member into separate
-        // non intrusive save/load member functions
-        template<class Archive, class Type, class Key, class Hash, class Compare, class Allocator >
-            inline void serialize(
-                                  Archive & ar,
-                                  boost::unordered_map<Key, Type, Hash, Compare, Allocator> &t,
-                                  const unsigned int file_version
-                                  ){
-            boost::serialization::split_free(ar, t, file_version);
-        }
-
-    } // serialization
-} // namespace boost
 
 namespace OpenNero
 {
@@ -120,7 +54,7 @@ namespace OpenNero
         }
     };
 
-	typedef boost::unordered_map<StateActionPair, double> StateActionDoubleMap;
+	typedef std::unordered_map<StateActionPair, double, boost::hash<StateActionPair>> StateActionDoubleMap;
 
 	/// An exact table-based approximator
     class TableApproximator : public Approximator
